0e0087bed6e0e155261c03e387201c2f7ec6f2df
santosh chaurasia <mac_santosh@santoshs-MacBook-Air.local>
2020-01-30 00:26:33 +0530

    implementation bubble sort

--- SpringFramework/src/main/java/com/spring/basics/SpringFramework/BubbleSortAlgorithm.java
+++ SpringFramework/src/main/java/com/spring/basics/SpringFramework/BubbleSortAlgorithm.java
@@ -6,6 +6,22 @@ import org.springframework.stereotype.Component;
 public class BubbleSortAlgorithm implements SortAlgorithm{
 	
 	public int [] sort(int [] numbers) {
+		
+		int n = numbers.length;
+		Boolean isSwap = false;
+		
+		for(int i=0;i<n-1;i++) {
+			for(int j = 0; j < n-1-i;j++) {
+				if(numbers[j] > numbers[j+1]) {
+					int temp = numbers[j];
+					numbers[j] = numbers[j+1];
+					numbers[j+1] = temp;
+					isSwap = true;
+				}
+			}
+			if (isSwap == false) //numbers are already sorted
+				break;
+		}
 		return numbers;
 	}
 	
