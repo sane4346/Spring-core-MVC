1c8fda4ea6c20984fec98b9186994858c6483393
Kumar Santosh <kumar@Admins-MacBook-Pro.local>
2020-03-02 11:06:32 +0530

    todo with spring mvc start

--- BasicWebAppJSP/src/main/java/webapp/Login/LoginServlet.java
+++ BasicWebAppJSP/src/main/java/webapp/Login/LoginServlet.java
@@ -14,9 +14,30 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(urlPatterns = "/login.do")
 public class LoginServlet extends HttpServlet{
 	
+	private UserValidationService service = new UserValidationService();
+	
 	  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		  
+		  //String name = request.getParameter("name");
+		  //request.setAttribute("userName", name);
 		  request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
+
+	  }
+	  
+	  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		  
+		  String name = request.getParameter("username");
+		  String password = request.getParameter("password");
+		  
+		  boolean isValidUser = service.isUserValid(name, password);
+		  
+		  if(isValidUser) {
+			  request.setAttribute("name", name);
+			  request.setAttribute("password", password);
+			  request.getRequestDispatcher("/WEB-INF/views/welcome.jsp").forward(request, response); 
+		  } else {
+			  request.setAttribute("errorMessage", "Invalid Credentials!");
+			  request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response); 
+		  }
 
 	  }
 
--- BasicWebAppJSP/src/main/webapp/WEB-INF/views/login.jsp
+++ BasicWebAppJSP/src/main/webapp/WEB-INF/views/login.jsp
@@ -1,3 +1,4 @@
+<%@ page import ="java.util.Date" %>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <!DOCTYPE html>
@@ -7,6 +8,10 @@
 <title>Guru84</title>
 </head>
 <body>
- My First JSP file
+<form action="/login.do" method="post">
+Enter Your Name <input type="text" name="username" /> <input type = "password" name="password"/>
+<input type="submit" value="Login" /> 
+<p><font color ="red">${errorMessage}</font></p>
+</form>
 </body>
 </html>
